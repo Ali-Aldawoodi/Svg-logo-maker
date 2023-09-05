2a9bddde4b891becd5b75ba921281792772ea308
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-09-05 12:17:55 -0600

    Added logo function content as well as defined the shape variables.

--- index.js
+++ index.js
@@ -27,47 +27,74 @@ const Shapes = require('./lib/shapes.js')
 const fs = require('fs')
 const inquirer = require('inquirer')
 
-// const shapes = new Shapes();
+// I need to create a logo function that within it contains code to use as data down below. What do i put into the function? It has to contain all of the data from the user input right? 
+
+const circle = '<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="25" cy="75" r="20"/>' 
+const triangle ='<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><polygon points="225,10 100,210 350,210"/>' 
+const square = '<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><rect width="300" height="100"/>'
+  
+
+function logo({ letters, letterColors, shape, color }) {
+    
+  const choiceIndex = [`${shape} fill = ${color} /> <text x="150" y="125" font-size="60" fill = ${letterColors}>${letters}</text></svg>`, `${shape} fill = ${color} /> <text x="150" y="125" font-size="60" fill = ${letterColors}/> ${letters}</text></svg>`, `<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><rect width="300" height="100" fill = ${color} /> <text x="150" y="125" font-size="60" fill = ${letterColors}/> ${letters}</text></svg>`]
+   
+   
 
 
+};
 
 const questions = [
     {
         type: 'input',
-        message:'Enter three letters that you would like to display on your logo.',
+        message: 'Enter three letters that you would like to display on your logo.',
         name: 'letters'
     },
     {
         type: 'input',
-        message:'What shape would you like your logo to be?',
-        name: 'shape'
+        message: 'What color would you like your logo text to be?',
+        name: 'letterColors'
     },
     {
         type: 'list',
-        message:'What color would you like your logo to be?',
-        choices:['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'white', 'black', 'brown', 'grey', 'pink'],
+        message: 'What shape would you like your logo to be?',
+        choices: ['circle', 'triangle', 'square'],
+        name: 'shape'
+    },
+    {
+        type: 'input',
+        message: 'What color would you like your logo to be?',
         name: 'color'
     },
 ]
 
-function writeToFile(filename,data) {
-    fs.writeFile(filename,data, (err) => 
-    err? console.log(err) : console.log('Check out your logo!'))
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) =>
+        err ? console.log(err) : console.log('Check out your logo!'))
 };
 
 function init() {
     inquirer
         .prompt(questions)
         .then((response) => {
-        console.log(response)
-        const data = logo(response)
+            console.log(response)
+            const data = logo(response)
 
-        
-        writeToFile('logo.svg', data, (err) =>
-            err ? console.log(err) : console.log('Generated logo.svg'))
-    })
+
+            writeToFile('logo.svg', data, (err) =>
+                err ? console.log(err) : console.log('Generated logo.svg'))
+        })
 }
 
 
 init();
 
+
+
+
+
+
+
+
+// const circle = `<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="25" cy="75" r="20" fill = ${color} /> </svg>`
+//     const triangle = `<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><polygon points="225,10 100,210 350,210" fill = ${color} /></svg>`
+//     const square = `<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg"><rect width="300" height="100" fill = ${color} /></svg>`
