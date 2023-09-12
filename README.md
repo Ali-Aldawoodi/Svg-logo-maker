0f4889fd6dd8c9375ba357f5ace3fa88ef929d24
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-09-11 18:00:45 -0600

    Added Readme including description of project as well as links to Github and video of the project.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Svg-logo-maker
+
+## Description
+User will be prompted to answer questions. Once the questions are answered the app will generate an SVG file with elements that the user is able to customize by ansering the prompts. There are also tests that run through the app using the Jest package. A separate 'Shapes' Javascript file has also been created to hold the three different class of shapes that the user can choose from. 
+
+## Collaborators 
+Trinh Nguyen - Tutor
+
+## Links 
+Video - https://drive.google.com/file/d/1rwe0D9wIBhVcxdT8JdGIZeAjX2Czc0bW/view
+Github - https://github.com/Ali-Aldawoodi/Svg-logo-maker
--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for text
-// THEN I can enter up to three characters
-
-
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-
-
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-
-
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// All of the above can be created like the previous challenge
-
-// WHEN I have entered input for all the prompts
-// THEN an SVG file is created named `logo.svg`
-// AND the output text "Generated logo.svg" is printed in the command line
-
-
-// WHEN I open the `logo.svg` file in a browser
-// THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
 const {Circle, Triangle, Square} = require('./shapes.js')
 const fs = require('fs')
 const inquirer = require('inquirer')
 
-// I need to create a logo function that within it contains code to use as data down below. What do i put into the function? It has to contain all of the data from the user input right? 
-
-// Use this function to create into an object with classes. Still keep conditionals but start using classes after
 function logo({ letters, letterColors, shape, color }) {
-    // if user selects circle return circle. 
     let shapeObject 
     if (shape ==='circle') {
         shapeObject = new Circle()
@@ -47,7 +18,6 @@ function logo({ letters, letterColors, shape, color }) {
     shapeObject.setColor(color)
     shapeObject.setLetterColors(letterColors)
     shapeObject.setLetters(letters)
-    console.log(shapeObject)
     return `<svg width="200" height="100" xmlns="http://www.w3.org/2000/svg">${shapeObject.render()}<text x="50%" y="50%" text-anchor="middle" font-size="10" fill = "${shapeObject.letterColors}">${shapeObject.letters}</text></svg>`
  
     
@@ -86,10 +56,8 @@ function init() {
     inquirer
         .prompt(questions)
         .then((response) => {
-            console.log(response)
             const data = logo(response)
 
-            console.log(data)
             writeToFile('logo.svg', data, (err) =>
                 err ? console.log(err) : console.log('Generated logo.svg'))
         })
